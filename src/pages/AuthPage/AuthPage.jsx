242b51c9da2e1fd0f2dde38a1838015d51afc808
TCOD <amirprogrammer38@gmail.com>
2025-01-10 20:54:54 +0330

    initial commit

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,11 @@
+import { Flex } from "antd";
+
+const AuthPage = () => {
+  return (
+    <Flex align="center" justify="center" style={{ minHeight: "100vh" }}>
+      Auth Page
+    </Flex>
+  );
+};
+
+export default AuthPage;
